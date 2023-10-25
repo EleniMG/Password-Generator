9ba4844db9f7e42434204345e5de1847e60a2339
Eleni <elenimariex@gmail.com>
2023-10-25 17:08:27 +0100

    Adding passwordLength prompt and pseudocode comments

--- script.js
+++ script.js
@@ -90,11 +90,16 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  var passwordLength = prompt("How long do you want your password to be? Select a number between 8 and 128")
+
+  // Create a function to test the user's input for passwordLength before going on to the other questions.
   var specialCharAmount = prompt("How many special characters would you like?");
   var numericCharAmount = prompt("How many numbers would you like?");
   var lowerCaseAmount = prompt("How many lowercased letters would you like?");
   var upperCaseAmount = prompt("How many uppercased letters would you like?");
-  console.log(specialCharAmount, numericCharAmount, lowerCaseAmount, upperCaseAmount)
+
+
+  // Save data into an object
 }
 
 // Function for getting a random element from an array
@@ -121,3 +126,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+
+// Once all prompts are answered, the password should be generated and displayed in an alert or written to the page.
