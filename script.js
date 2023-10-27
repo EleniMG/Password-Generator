c6fbef505fc768d1e99389c7a0f82955f614f5cf
Eleni <elenimariex@gmail.com>
2023-10-27 11:59:38 +0100

    Separating the getPasswordOptions function to check for the password length first then character check

--- script.js
+++ script.js
@@ -95,42 +95,47 @@ function getPasswordOptions() {
   var passwordLength = prompt("How long do you want your password to be? Select a number between 8 and 128")
 
   // Create a function/conditional if statement to test the user's input for passwordLength before going on to the other questions.
+    // Need to turn the passwordLength value from a string to a number
 
   if (passwordLength >= 8 && passwordLength <= 128){
-    var numericCharCheck = confirm("Would you like to include numbers?");
-    var lowerCaseCheck = confirm("Would you like to include lowercase letters?");
-    var upperCaseCheck = confirm("Would you like to include uppercase letters?");
-    var specialCharCheck = confirm("Would you like to include special characters? E.g. *, !, &, %, $, £");
+    // characterCheck();
+    var passwordLengthNum = parseInt(passwordLength)
+    return passwordLengthNum;
   
   } else {
     alert("Please select a number between 8 and 128")
     getPasswordOptions()
   }
+}
+
+// Function to check which types of characters the user wants to include, and return those values.
+function characterCheck(){
+
+    var numericCharCheck = confirm("Would you like to include numbers?");
+    var lowerCaseCheck = confirm("Would you like to include lowercase letters?");
+    var upperCaseCheck = confirm("Would you like to include uppercase letters?");
+    var specialCharCheck = confirm("Would you like to include special characters? E.g. *, !, &, %, $, £");
 
   // Create a function/conditional if statement to check one type of character has been selected.
 
   if(numericCharCheck === false && lowerCaseCheck === false && upperCaseCheck === false && specialCharCheck === false){
     alert("Please select a character type");
-    getPasswordOptions()
+    characterCheck()
   }
 
   // Save data into an object
 
   var userSelections = {
-    passwordLength: passwordLength,
     numericCharCheck: numericCharCheck,
     lowerCaseCheck: lowerCaseCheck,
     upperCaseCheck: upperCaseCheck,
     specialCharCheck: specialCharCheck
   }
 
-  // console.log(userSelections)
+  return userSelections
+}
 
-  // Need to turn the passwordLength value from a string to a number
-
-  var passwordLengthNum = parseInt(Object.values(userSelections)[0])
-
-  // console.log(typeof passwordLengthNum)
+console.log(getPasswordOptions(), characterCheck())
 
   // Create new array to store the characters that will go into the password
   // Create new array which contains the characters that could be selected for the password
@@ -138,55 +143,53 @@ function getPasswordOptions() {
 
 
 
-  var possibleCharacters = [];
+//   var possibleCharacters = [];
 
-  if (numericCharCheck){
-    possibleCharacters.push(numericCharacters)
-  }
+//   if (numericCharCheck){
+//     possibleCharacters.push(numericCharacters)
+//   }
 
-  if (specialCharCheck){
-    possibleCharacters.push(specialCharacters)
-  }
+//   if (specialCharCheck){
+//     possibleCharacters.push(specialCharacters)
+//   }
 
-  if (lowerCaseCheck){
-    possibleCharacters.push(lowerCasedCharacters)
-  }
+//   if (lowerCaseCheck){
+//     possibleCharacters.push(lowerCasedCharacters)
+//   }
 
-  if (upperCaseCheck){
-    possibleCharacters.push(upperCasedCharacters)
-  }
+//   if (upperCaseCheck){
+//     possibleCharacters.push(upperCasedCharacters)
+//   }
 
-  var allCharacters = possibleCharacters.flat(Infinity)
+//   var allCharacters = possibleCharacters.flat(Infinity)
 
-  return {
-    allCharacters, 
-    passwordLengthNum
-  };
+//   return {
+//     allCharacters, 
+//     passwordLengthNum
+//   };
 
-}
+// // console.log(getPasswordOptions())
 
-// console.log(getPasswordOptions())
+// var characterList = getPasswordOptions().allCharacters;
+// var passwordLengthCount = getPasswordOptions().passwordLengthNum;
 
-var characterList = getPasswordOptions().allCharacters;
-var passwordLengthCount = getPasswordOptions().passwordLengthNum;
+// var shortlistedCharacters = [];
 
-var shortlistedCharacters = [];
+// // Function for getting a random element from an array
+// function getRandom(arr) {
+//   getPasswordOptions()
 
-// Function for getting a random element from an array
-function getRandom(arr) {
-  getPasswordOptions()
-
-  for (var i = 1; i <= passwordLengthCount; i++){
-    var index = Math.floor(Math.random() * arr.length)
+//   for (var i = 1; i <= passwordLengthCount; i++){
+//     var index = Math.floor(Math.random() * arr.length)
         
-    shortlistedCharacters.push(arr[index])
+//     shortlistedCharacters.push(arr[index])
     
-  }
-  return shortlistedCharacters;
+//   }
+//   return shortlistedCharacters;
 
-}
+// }
 
-shortlistedCharacters = getRandom(characterList)
+// shortlistedCharacters = getRandom(characterList)
 
 // Function to generate password with user input
 function generatePassword() {
@@ -198,7 +201,6 @@ var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  getRandom(characterList);
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
 
